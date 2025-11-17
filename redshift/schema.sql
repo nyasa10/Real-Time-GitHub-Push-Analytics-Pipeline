db12f57850e27c8acf164acf3ef689cdd81cc279
Nyasa Dwivedi <168264046+nyasa10@users.noreply.github.com>
2025-11-17 13:31:45 +0530

    Update schema.sql

--- redshift/schema.sql
+++ redshift/schema.sql
@@ -1,1 +1,10 @@
+DROP TABLE IF EXISTS public.github_pushes;
 
+CREATE TABLE github_pushes (
+    id VARCHAR(255) DISTKEY NOT NULL,
+    type VARCHAR(50),
+    actor_name VARCHAR(100),
+    actor_login VARCHAR(100),
+    repo_name VARCHAR(100),
+    payload_ref VARCHAR(255)
+);
